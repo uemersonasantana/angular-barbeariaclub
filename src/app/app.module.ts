fdd42d45298eed5f2192666409a9ee1a2db1f8c2
Uemerson Santana <uemerson@icloud.com>
2020-04-03 10:52:26 -0300

    Primeiro commit app angular da Barbearia

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,104 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+
+import {MatCardModule} from '@angular/material/card';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatBadgeModule} from '@angular/material/badge';
+
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatNativeDateModule, MatRippleModule} from '@angular/material/core';
+
+import { HttpClientModule } from '@angular/common/http';
+
+import { AgendamentoComponent } from './components/agendamento/agendamento.component';
+import { NovoAgendamentoComponent } from './components/novo-agendamento/novo-agendamento.component';
+
+import { AgendamentoService } from './services/agendamento.service';
+import { BarbeiroService } from './services/barbeiro.service';
+
+import { TimePickerComponent } from './components/time-picker/time-picker.component';
+import {OverlayModule} from '@angular/cdk/overlay';
+
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+
+/*const routes: Routes = [
+  { path: '', component: AgendamentoComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];*/
+
+registerLocaleData(localePt);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendamentoComponent,
+    NovoAgendamentoComponent,
+    TimePickerComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //RouterModule.forRoot(routes),
+    
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatGridListModule,
+    MatTableModule,
+    MatFormFieldModule,
+
+    FormsModule, 
+    ReactiveFormsModule,
+    HttpClientModule,
+    OverlayModule,
+
+    MatInputModule,
+    MatSelectModule,
+    MatCardModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatChipsModule,
+    MatBadgeModule,
+    MatAutocompleteModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRippleModule
   ],
-  providers: [],
+  entryComponents: [
+    NovoAgendamentoComponent,
+    TimePickerComponent
+  ],
+  providers: [
+    AgendamentoService,
+    BarbeiroService,
+    [{ provide: LOCALE_ID, useValue: 'pt-BR' }]
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
---
+++ src/app/components/novo-agendamento/novo-agendamento.component.ts
@@ -0,0 +1,116 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {MatDialogRef} from '@angular/material/dialog';
+import {Barbeiro} from '../../modal/barbeiro';
+import {BarbeiroService} from '../../services/barbeiro.service';
+import {Agendamento,AgendamentoService} from '../../services/agendamento.service';
+
+import { debounceTime, tap, switchMap, finalize } from 'rxjs/operators';
+
+const API_URL: string = 'http://localhost:8000/api';
+
+@Component({
+  selector: 'app-novo-agendamento',
+  templateUrl: './novo-agendamento.component.html',
+  styleUrls: ['./novo-agendamento.component.css']
+})
+export class NovoAgendamentoComponent implements OnInit {
+
+  barbeiros: Barbeiro[];
+  agendamento: Agendamento = {
+    id: 0,
+    descricao: '',
+    dataagendamento: null,
+    cliente_id: 0,
+    barbeiro_id: 0
+  };
+
+  searchClientsCtrl = new FormControl();
+  filteredClients: any;
+  isLoading = false;
+  errorMsg: string;
+  isOpen = false;
+
+  htmlOfErrors:any;
+
+  constructor(
+    private AgendamentoService: AgendamentoService,
+    private BarbeiroService: BarbeiroService,
+    private http: HttpClient,
+    public dialogref: MatDialogRef<NovoAgendamentoComponent>
+  ) {}
+
+  ngOnInit(): void {
+    this.barbeiros = this.BarbeiroService.barbeiros;
+  
+    this.searchClientsCtrl.valueChanges
+      .pipe(
+        debounceTime(500),
+        tap(() => {
+          this.errorMsg = "";
+          this.filteredClients = [];
+          this.isLoading = true;
+        }),
+        switchMap(value => this.http.get(API_URL+"/clientes/"+value)
+          .pipe(
+            finalize(() => {
+              this.isLoading = false
+            }),
+          )
+        )
+      )
+      .subscribe(data => {
+        this.filteredClients = data;
+        /*if (data['Search'] == undefined) {
+          this.errorMsg = data['Error'];
+          this.filteredClients = [];
+        } else {
+          this.errorMsg = "";
+          this.filteredClients = data['Search'];
+        }*/
+      });
+  }
+
+  public onSetDate(newDate: Date) {
+    this.isOpen = false;
+    this.agendamento.dataagendamento = newDate;
+  }
+
+  
+  salvar() {
+    this.agendamento.empresa_id = 1;
+    this.agendamento.user_id = 1
+  
+    this.AgendamentoService
+        .addAgendamentos(this.agendamento)
+        .subscribe(
+            agendamentos => {
+              this.dialogref.close();
+            },
+            error => {
+              this.htmlOfErrors = '<ul>';
+              
+              for(let e of Object.keys(error.error.errors) ) {
+                this.htmlOfErrors += "<li>"+error.error.errors[e][0]+"</li>";
+              }
+
+              this.htmlOfErrors += '</ul>';
+              //<any>error
+            }
+        );
+  }
+  cancelar() {
+    this.dialogref.close(null);
+  }
+
+  selecionarCliente(id) {
+    if ( this.searchClientsCtrl.value != '' ) {
+      this.agendamento.cliente_id = id;
+    }
+  }
+
+  limparIdCliente() {
+    this.agendamento.cliente_id = 0;
+  }
+}
