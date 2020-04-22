e5b3b0fe16d859990605536346968513abeb77fc
Uemerson Santana <uemerson@icloud.com>
2020-04-22 10:52:13 -0300

    Correção filtro de notificações.

--- src/app/services/agendamento.service.ts
+++ src/app/services/agendamento.service.ts
@@ -41,31 +41,32 @@ export class AgendamentoService {
     this._http.post(API_URL + '/agendamentos/find', tempForm).subscribe(
       (data: any[]) => {
         for(let d of data) {
-          //console.log(d.cliente.sobrenome.length)
-          let sobrenome:string = ''
-          let Fone2:string = ''
+          if (d.cliente.nome) {
+            let sobrenome:string = ''
+            let Fone2:string = ''
 
-          if ( d.cliente.sobrenome ) {
-            sobrenome = d.cliente.sobrenome.substr(0,6)
-            if ( d.cliente.sobrenome.length > 6 ) {
-              sobrenome += '...'
-            } 
-          } else {
-            sobrenome = ''
-          }
+            if ( d.cliente.sobrenome ) {
+              sobrenome = d.cliente.sobrenome.substr(0,6)
+              if ( d.cliente.sobrenome.length > 6 ) {
+                sobrenome += '...'
+              } 
+            } else {
+              sobrenome = ''
+            }
 
-          if (Fone2) {
-            Fone2 = ' | '+'Fone 2: '+d.cliente.fone2
+            if (Fone2) {
+              Fone2 = ' | '+'Fone 2: '+d.cliente.fone2
+            }
+            
+            this.Notfiy.confirm('Fone 1: '+d.cliente.fone1 + Fone2, d.cliente.nome + ' ' + sobrenome, {
+              showProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              buttons: [
+                {text: 'Fechar', action: (toast) => {this.Notfiy.remove(toast.id); }, bold: false},
+              ]
+            });
           }
-          
-          this.Notfiy.confirm('Fone 1: '+d.cliente.fone1 + Fone2, d.cliente.nome + ' ' + sobrenome, {
-            showProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            buttons: [
-              {text: 'Fechar', action: (toast) => {this.Notfiy.remove(toast.id); }, bold: false},
-            ]
-          });
         }
       }
     )
